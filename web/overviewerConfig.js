ed5b7f728cb2555dfc7fce0025b8ea2ea1ad67ee
Jens Møller <jens@jensz12.com>
2016-06-02 15:16:21 +0200

    Create overviewerConfig.js

---
+++ web/overviewerConfig.js
@@ -0,0 +1,110 @@
+var overviewerConfig = {
+    "worlds": [
+        "LeQuest - Overworld", 
+        "LeQuest - Nether"
+    ], 
+    "tilesets": [
+        {
+            "spawn": [
+                89, 
+                64, 
+                18
+            ], 
+            "isOverlay": false, 
+            "last_rendertime": 1464821994, 
+            "name": "Overworld", 
+            "poititle": "Markers", 
+            "north_direction": 0, 
+            "minZoom": 0, 
+            "bgcolor": "#1a1a1a", 
+            "zoomLevels": 9, 
+            "base": "", 
+            "imgextension": "png", 
+            "defaultZoom": 1, 
+            "world": "LeQuest - Overworld", 
+            "maxZoom": 9, 
+            "path": "lequestoverworld", 
+            "showlocationmarker": true
+        }, 
+        {
+            "spawn": [
+                89, 
+                64, 
+                18
+            ], 
+            "isOverlay": false, 
+            "last_rendertime": 1464821994, 
+            "name": "Cave", 
+            "poititle": "Markers", 
+            "north_direction": 0, 
+            "minZoom": 0, 
+            "bgcolor": "#1a1a1a", 
+            "zoomLevels": 9, 
+            "base": "", 
+            "imgextension": "png", 
+            "defaultZoom": 1, 
+            "world": "LeQuest - Overworld", 
+            "maxZoom": 9, 
+            "path": "lequestcave", 
+            "showlocationmarker": true
+        }, 
+        {
+            "spawn": [
+                56, 
+                64, 
+                -38
+            ],  
+            "isOverlay": false, 
+            "last_rendertime": 1464795576, 
+            "name": "Nether", 
+            "poititle": "Markers", 
+            "north_direction": 0, 
+            "minZoom": 0, 
+            "bgcolor": "#1a1a1a", 
+            "zoomLevels": 9, 
+            "base": "", 
+            "imgextension": "png", 
+            "defaultZoom": 1, 
+            "world": "LeQuest - Nether", 
+            "maxZoom": 9, 
+            "path": "lequestnether", 
+            "showlocationmarker": true
+        }
+    ], 
+    "CONST": {
+        "mapDivId": "mcmap", 
+        "UPPERLEFT": 0, 
+        "tileSize": 384, 
+        "UPPERRIGHT": 1, 
+        "image": {
+            "defaultMarker": "signpost.png", 
+            "queryMarker": "map_marker.png", 
+            "signMarker": "signpost_icon.png", 
+            "spawnMarker": "home.png", 
+            "bedMarker": "bed.png"
+        }, 
+        "LOWERRIGHT": 2, 
+        "LOWERLEFT": 3, 
+        "regionStrokeWeight": 2
+    }, 
+    "map": {
+        "debug": true, 
+        "north_direction": "lower-left", 
+        "controls": {
+            "spawn": true, 
+            "mapType": true, 
+            "compass": true, 
+            "coordsBox": true, 
+            "overlays": true, 
+            "searchBox": true, 
+            "zoom": true, 
+            "pan": true
+        }, 
+        "center": [
+            -314, 
+            67, 
+            94
+        ], 
+        "cacheTag": "1464867141"
+    }
+};
