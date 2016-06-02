63a167c0f6ea235ab4d210c30b41b4077f72cd6c
Jens Møller <jens@jensz12.com>
2016-06-02 14:03:04 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,4 @@
-# This is a sample config file, meant to give you an idea of how to format your
-# config file and what's possible.
-
-# Define the path to your world here. 'My World' in this case will show up as
+# Define the path to your world here. 'LeQuest' in this case will show up as
 # the world name on the map interface. If you change it, be sure to also change
 # the referenced world names in the render definitions below.
 worlds['LeQuest'] = "C:/Users/Jens/Downloads/overviewer/LeQuest"
@@ -9,24 +6,21 @@ worlds['LeQuest'] = "C:/Users/Jens/Downloads/overviewer/LeQuest"
 # Define where to put the output here.
 outputdir = "C:/world"
 
-# This is an item usually specified in a renders dictionary below, but if you
-# set it here like this, it becomes the default for all renders that don't
-# define it.
-# Try "smooth_lighting" for even better looking maps!
-rendermode = "smooth_lighting"
-
+# Overworld med Smooth Lightning!
 renders["lequestoverworld"] = {
     "world": "LeQuest",
     "title": "Overworld",
     "rendermode": smooth_lighting,
     "dimension": "overworld",
 }
+# Overworld caves
 renders["lequestcave"] = {
     "world": "LeQuest",
     "title": "Cave",
     "rendermode": cave,
     "dimension": "overworld",
 }
+# Nether med Smooth Lightning
 renders["lequestnether"] = {
     "world": "LeQuest",
     "title": "Nether",
